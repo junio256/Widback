7fbfa08f4b0d5d86d6d99aad3af73be3e2762c6f
Junio KOI <juniokoi@hotmail.com>
2022-05-11 18:07:04 -0300

    Deletado pasta NPM_Package

--- NPM_Package/src/components/WidgetForm/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleNotch } from "phosphor-react";
-
-export function Loading() {
-  return (
-    <div className="w-6 h-6 items-center justify-center flex overflow-hidden">
-      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
-    </div>
-  );
-}
