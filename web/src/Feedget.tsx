ed80b7c80549042718f90f2ca991cf794d718321
Junio KOI <juniokoi@hotmail.com>
2022-05-11 18:14:05 -0300

    Renomeado arquivos para  incluir em package

--- web/src/Feedget.tsx
+++ web/src/Feedget.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './Global.css';
+import { WidgetButton } from "./components/WidgetButton";
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
 
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export function Feedget() {
+  return <WidgetButton />
+}
---
+++ web/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Feedget } from './Feedget'
+import './Global.css';
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Feedget />
+  </React.StrictMode>
+)
