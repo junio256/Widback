fb34e0e5d582dc47af0dd3d7868b0290519ed8f9
Junio KOI <juniokoi@hotmail.com>
2022-05-11 03:00:40 -0300

    Add more style to email

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -37,11 +37,42 @@ export class SubmitFeedbackUseCase {
     await this.mailAdapter.sendMail({
       subject: 'Novo feedback',
       body: [
-        `<div style="font-family: sans-serif; font-size: 16px; color:#111;">`,
-        `<p>Tipo do feedback: ${type}</p>`,
-        `<p>Comentário: ${comment}</p>`,
-        `<a target="_blank" href="${screenshot}">Abra a foto em uma nova guia</a>`,
-        screenshot ? `${screenshot}` : ``,
+        `<div 
+        style="
+        font-family: Helvetica, Arial, sans-serif; 
+        font-size: 1rem;
+        background-color: #EFF2F8 ; 
+        color:#161320; 
+        border-radius: 16px; 
+        -moz-box-shadow: -1px 1px 21px -2px rgba(0, 0, 0, 0.5);  
+        -webkit-box-shadow: -1px 1px 21px -2px rgba(0, 0, 0, 0.5); 
+        box-shadow: -1px 1px 21px -2px rgba(0, 0, 0, 0.5)
+        ">`,
+
+
+        `<h2>Um novo Feedback foi emitido!</h2>`,
+
+
+        `<hr style="
+        width: 1rem; 
+        border: 0; 
+        height: 2px; 
+        border-radius: 4px; 
+        background-color: #996DFF;
+        ">`,
+
+
+        `<p><strong>Tipo do feedback:</strong> ${type}</p>`,
+        `<p><strong>Comentário:</strong> ${comment}</p>`,
+
+        screenshot
+          ?
+          `<a target="_blank" href="${screenshot}">
+          Abra a foto em <em>alta resolução</em> em uma nova guia.
+          </a> <br> 
+          <img style="max-width: 600px;" src="${screenshot}" />`
+          :
+          `Usuário não tirou nenhuma foto`,
         `</div>`
       ].join('\n')
     })
