3678f74557b4ab6120773e5b1ddeeb3c61c8e8fa
Junio KOI <juniokoi@hotmail.com>
2022-05-14 17:30:10 -0300

    Backend Adjusts

--- server/directus.config.js
+++ server/directus.config.js
@@ -7,7 +7,7 @@ module.exports = function (env) {
     SECRET: env.SECRET,
 
     // Reference: https://docs.railway.app/deploy/exposing-your-app
-    PORT: env("DATABASE_URL"),
+    PORT: env.PORT,
 
     // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
     PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
