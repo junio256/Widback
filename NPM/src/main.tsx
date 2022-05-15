d7b950cfafb64e6930fd4afb289f5b2c49020636
Junio KOI <juniokoi@hotmail.com>
2022-05-15 15:58:48 -0300

    teste

---
+++ NPM/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Feedget } from '.'
+import './global.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Feedget />
+  </React.StrictMode>
+)
