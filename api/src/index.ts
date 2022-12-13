1a6a97d2922f769c6447ae83f241677c9a3265aa
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-12-12 23:33:35 -0300

    build: change source from server to index

--- api/src/index.ts
+++ api/src/index.ts
@@ -16,7 +16,7 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'https://widback.vercel.app',
-        'https://widback.juniokoi.dev/']
+        'https://widback.juniokoi.dev']
 }))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
