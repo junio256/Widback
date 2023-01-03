876a39e6966ffcc7b9b6d76e39adda558950fb77
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2023-01-03 18:49:44 -0300

    feat: remove cors Origins from source code

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 import {routes} from './routes';
 import cors from 'cors';
+const env = require('dotenv').config();
 
 const app = express();
+const ORIGINS = [
+    'http://localhost:3000',
+    env.ORIGINS
+]
 // GET, POST, PUT, PATCH, DELETE
 
 //GET = Buscar informações
@@ -13,14 +18,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'https://widback.vercel.app',
-        'https://widbacktest.vercel.app',
-        'https://widback.juniokoi.dev']
+    origin: ORIGINS
 }))
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", ORIGINS);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 })
